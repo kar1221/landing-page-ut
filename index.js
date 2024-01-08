847685fdeb39f9b1c288f43725efba8977cc7147
kar1221 <khyeong191@gmail.com>
2024-01-08 14:44:36 +0800

    add the rest of the page and background music

--- index.js
+++ index.js
@@ -2,10 +2,43 @@ let navBarMenu = $(".link-container li");
 
 navBarMenu.mouseenter(() => {
   let audio = new Audio("res/snd_select.wav");
+  audio.volume = 0.2;
   audio.play();
 });
 
 navBarMenu.mousedown(() => {
   let audio = new Audio("res/snd_save.wav");
+  audio.volume = 0.2;
   audio.play();
+});
+
+/* The Credit Panel */
+let creditPanel = $(".credit-panel");
+let creditButton = $(".link-container .credit a");
+let closeButton = $(".close-button");
+creditButton.mousedown(() => {
+  creditPanel.addClass("active");
+});
+
+closeButton.mousedown(() => {
+  let audio = new Audio("res/snd_save.wav");
+  audio.volume = 0.2;
+  audio.play();
+  creditPanel.removeClass("active");
+});
+
+/* Toby Bark */
+let tobyFox = $(".toby-fox img");
+tobyFox.mousedown(() => {
+  let audio = new Audio("res/doge.mp3");
+  audio.volume = 0.2;
+  audio.play();
+});
+
+/* Background music */
+$(document).ready(() => {
+  let backgroundMusic = new Audio("res/quiet-water.mp3");
+  backgroundMusic.volume = 0.1;
+  backgroundMusic.loop = true;
+  backgroundMusic.play();
 });
